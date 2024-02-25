a7bae812d3c03e8aa1c18dddc1e29ba5910aa2d7
Shingo Nakanishi <sin7k24@hotmail.com>
2024-02-25 18:37:50 +0900

    done.

--- src/controllers/imgController.js
+++ src/controllers/imgController.js
@@ -1,14 +1,32 @@
-const fs = require("fs");
+const path = require("path");
 
+// Parse multipart/form-data.
+const multer = require("multer");
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        // Save to img directory.
+        cb(null, "img");
+    },
+    filename: function (req, file, cb) {
+        // Uploaded filename is upload_file_{Date}.orgext.
+        cb(
+            null,
+            file.fieldname + "_" + Date.now() + path.extname(file.originalname)
+        );
+    },
+});
+var upload = multer({ storage: storage }).single("upload_file");
 
 async function post(req, res, next) {
-    console.log(req.body);
-    console.log(req.body.name);
-    console.log(req.file);
-    console.log('imgController#post called.');
-    res.send("OK");
+    console.log("imgController#post called.");
+    upload(req, res, function (err) {
+        if(err) {
+            return res.send("Error uploading file.");
+        }
+        res.send(req.file.path);
+    });
 }
 
 module.exports = {
-    post
+    post,
 };
--- src/routes/apiRouter.js
+++ src/routes/apiRouter.js
@@ -1,40 +1,27 @@
-const express = require('express');
-const path = require('path'); 
-const md2HtmlController = require('../controllers/md2HtmlController');
-const mdController = require('../controllers/mdController');
-const searchController = require('../controllers/searchController');
-const historyController = require('../controllers/historyController');
-const imgController = require('../controllers/imgController');
+const express = require("express");
+const path = require("path");
+const md2HtmlController = require("../controllers/md2HtmlController");
+const mdController = require("../controllers/mdController");
+const searchController = require("../controllers/searchController");
+const historyController = require("../controllers/historyController");
+const imgController = require("../controllers/imgController");
 
 const router = express.Router();
 const myLogger = function (req, res, next) {
-    console.log('LOGGED : ', req.originalUrl)
-    next()
-  }
+    console.log("LOGGED : ", req.originalUrl);
+    next();
+};
 router.use(myLogger);
 
+router.get("/md2html/:year?/:month?", md2HtmlController.get);
 
-var multer  = require('multer');
-// 格納場所と新しくつけるファイル名の定義
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'img');
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.fieldname + '_' + Date.now() + path.extname(file.originalname));
-  }
-});
-var upload = multer({ storage: storage });
+router.get("/md/:year/:month/:day", mdController.get);
+router.post("/md", mdController.post);
 
-router.get('/md2html/:year?/:month?', md2HtmlController.get);
+router.post("/img", imgController.post);
 
-router.get('/md/:year/:month/:day', mdController.get);
-router.post('/md', mdController.post);
+router.post("/search", searchController.post);
 
-router.post('/img', upload.single('name'), imgController.post);
-
-router.post('/search', searchController.post);
-
-router.get('/years', historyController.get);
+router.get("/years", historyController.get);
 
 module.exports = router;
